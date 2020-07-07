24248335ce5ad7b88ddee2842d02b5e91354c1f5
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-07-07 19:46:31 +0300

    testing success message

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -8,3 +8,15 @@ it('has a text input', async () => {
 
 	assert(input);
 });
+
+it('Shows a success message with a valid email', async () => {
+	const dom = await render('index.html');
+
+	const input = dom.window.document.querySelector('input');
+	input.value = 'gosjgs@kgkskg.com';
+	dom.window.document.querySelector('form').dispatchEvent(new dom.window.Event('submit'));
+
+	const h1 = dom.window.document.querySelector('h1');
+
+	console.log('Contents of h1:', h1.innerHTML);
+});
